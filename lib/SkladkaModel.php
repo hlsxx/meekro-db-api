4448c698a89064bec1f87f84175e676faa64c5ea
Holes70 <holespato@gmail.com>
2022-10-10 20:15:04 +0200

    getMixedDataValue

--- lib/SkladkaModel.php
+++ lib/SkladkaModel.php
@@ -39,6 +39,7 @@ class SkladkaModel extends Model {
     }
 
     return $this->getSkladkyData(
+      $vsetkySkladky,
       DB::query("
         SELECT 
           skladky.*,
@@ -55,33 +56,62 @@ class SkladkaModel extends Model {
     );
   }
 
-  public function getSkladkyData(array $mixedData) : array {
+  /**
+   * @param array $mixedData
+   * @return array skladky data
+   */
+  public function getSkladkyData(array $vsetkySkladky, array $mixedData) : array {
     $skladky = [];
-    foreach ($mixedData as $item) {
-      if (!isset($skladky[$item["id"]])) {
-        $skladky[$item["id"]] = array_merge([
-          "id" => $item["id"],
-          "okres" => $item["okres"],
-          "nazov" => $item["nazov"],
-          "obec" => $item["obec"],
-          "trieda" => $item["trieda"],
-          "prevadzkovatel" => $item["prevadzkovatel"],
-          "sidlo" => $item["sidlo"],
-          "rok_zacatia" => $item["rok_zacatia"],
-          "typ" => $item["typ"],
-          "pocet_nahlaseni" => $item["pocet_nahlaseni"],
-          "existujuca" => $item["existujuca"],
-          "lat" => $item["lat"],
-          "lng" => $item["lng"]
-        ], Helper::getSkladkaTyp($item["typ_skladky"], $item["pocet_potvrdeni"]));
+
+    $mixedDataIds = [];
+    $mixedDataImproved = [];
+    foreach ($mixedData as $mixedItem) {
+      $mixedDataIds[] = $mixedItem["id"];
+      $mixedDataImproved[$mixedItem["id"]] = $mixedItem;
+    }
+
+    foreach ($vsetkySkladky as $skladka) {
+      if (in_array($skladka["id"], $mixedDataIds)) {
+        if (!isset($skladky[$skladka["id"]])) {
+          $skladky[$skladka["id"]] = array_merge(
+            [
+              "id" => $this->getMixedDataValue($mixedDataImproved, "id", $skladka["id"]),
+              "okres" => $this->getMixedDataValue($mixedDataImproved, "okres", $skladka["id"]),
+              "nazov" => $this->getMixedDataValue($mixedDataImproved, "nazov", $skladka["id"]),
+              "obec" => $this->getMixedDataValue($mixedDataImproved, "obec", $skladka["id"]),
+              "trieda" => $this->getMixedDataValue($mixedDataImproved, "trieda", $skladka["id"]),
+              "prevadzkovatel" => $this->getMixedDataValue($mixedDataImproved, "prevadzkovatel", $skladka["id"]),
+              "sidlo" => $this->getMixedDataValue($mixedDataImproved, "sidlo", $skladka["id"]),
+              "rok_zacatia" => $this->getMixedDataValue($mixedDataImproved, "rok_zacatia", $skladka["id"]),
+              "typ" => $this->getMixedDataValue($mixedDataImproved, "typ", $skladka["id"]),
+              "pocet_nahlaseni" => $this->getMixedDataValue($mixedDataImproved, "pocet_nahlaseni", $skladka["id"]),
+              "existujuca" => $this->getMixedDataValue($mixedDataImproved, "existujuca", $skladka["id"]),
+              "lat" => $this->getMixedDataValue($mixedDataImproved, "lat", $skladka["id"]),
+              "lng" => $this->getMixedDataValue($mixedDataImproved, "lng", $skladka["id"]),
+            ], 
+            Helper::getSkladkaTyp(
+              $this->getMixedDataValue($mixedDataImproved, "typ_skladky", $skladka["id"]),
+              $this->getMixedDataValue($mixedDataImproved, "pocet_potvrdeni", $skladka["id"])
+            )
+          );
+        } else {
+          $skladky[$skladka["id"]] = array_merge(
+            $skladky[$skladka["id"]],
+            Helper::getSkladkaTyp(
+              $this->getMixedDataValue($mixedDataImproved, "typ_skladky", $skladka["id"]),
+              $this->getMixedDataValue($mixedDataImproved, "pocet_potvrdeni", $skladka["id"])
+            )
+          );
+        }
       } else {
-        $skladky[$item["id"]] = array_merge(
-          $skladky[$item["id"]],
-          Helper::getSkladkaTyp($item["typ_skladky"], $item["pocet_potvrdeni"])
-        );
+        $skladky[$skladka["id"]] = $skladka;
       }
     }
 
     return array_values($skladky);
+  }
+
+  public function getMixedDataValue(array $mixedDataImproved, string $itemColumn, int $skladkaId) {
+    return $mixedDataImproved[$skladkaId][$itemColumn];
   }
 }
