ac9825c0a5841be7ea0641af4c45cc47b20ce5a8
Holes70 <holespato@gmail.com>
2022-11-02 17:47:17 +0100

    install

--- install.php
+++ install.php
@@ -25,37 +25,37 @@ $notifications->created_at = date('Y-m-d H:i:s', time());
 R::store($notifications);
 
 /** UCM_SKLADKY */
-$skladky = R::ucmdispense('ucm_skladky');
+$skladka1 = R::ucmdispense('ucm_skladky');
 
-$skladky->okres = 'Legalna skladka #1';
-$skladky->nazov = 'Testovacia notifikacia popis #1';
-$skladky->obec = 'Trnava';
-$skladky->trieda = 'XXXX';
-$skladky->prevadzkovatel = 'Legalna skladka prevadzkovatel #1';
-$skladky->sidlo = 'Legalna skladka sidlo #1';
-$skladky->rok_zacatia = date('Y-m-d H:i:s', time());
-$skladky->typ = 1;
-$skladky->pocet_nahlaseni = 0;
-$skladky->existujuca = 1;
-$skladky->lat = 48.1833;
-$skladky->lng = 17.0379;
+$skladka1->okres = 'Legalna skladka #1';
+$skladka1->nazov = 'Testovacia notifikacia popis #1';
+$skladka1->obec = 'Trnava';
+$skladka1->trieda = 'XXXX';
+$skladka1->prevadzkovatel = 'Legalna skladka prevadzkovatel #1';
+$skladka1->sidlo = 'Legalna skladka sidlo #1';
+$skladka1->rok_zacatia = date('Y-m-d H:i:s', time());
+$skladka1->typ = 1;
+$skladka1->pocet_nahlaseni = 0;
+$skladka1->existujuca = 1;
+$skladka1->lat = 48.1833;
+$skladka1->lng = 17.0379;
 
-R::store($skladky);
+R::store($skladka1);
 
-$skladky->okres = 'Nelegalna skladka #1';
-$skladky->nazov = 'Testovacia notifikacia popis #1';
-$skladky->obec = 'Trnava';
-$skladky->rok_zacatia = date('Y-m-d H:i:s', time());
-$skladky->typ = 2;
-$skladky->existujuca = 1;
-$skladky->lat = 48.384046250301;
-$skladky->lng = 17.587909698486;
+$skladka2 = R::ucmdispense('ucm_skladky');
 
-R::store($skladky);
+$skladka2->okres = 'Nelegalna skladka #1';
+$skladka2->nazov = 'Testovacia notifikacia popis #1';
+$skladka2->obec = 'Trnava';
+$skladka2->rok_zacatia = date('Y-m-d H:i:s', time());
+$skladka2->typ = 2;
+$skladka2->existujuca = 1;
+$skladka2->lat = 48.384046250301;
+$skladka2->lng = 17.587909698486;
+
+R::store($skladka2);
 
 /** UCM_SKLADKY_TYPY */
-$skladkyTypy = R::ucmdispense('ucm_skladky_typy');
-
 $skladkaTypyArray = [
   'Bio odpad',
   'Papier',
@@ -68,13 +68,53 @@ $skladkaTypyArray = [
 ];
 
 foreach ($skladkaTypyArray as $skladkaTyp) {
+  $skladkyTypy = R::ucmdispense('ucm_skladky_typy');
   $skladkyTypy->nazov = $skladkaTyp;
   R::store($skladkyTypy);
 }
 
 /** UCM_SKLADKY_TYPY_CROSS */
-$skladky = R::ucmdispense('ucm_skladky_cross');
+$skladkaTypCross = R::ucmdispense('ucm_skladky_cross');
 
-$skladky->id_skladka = 'Legalna skladka #1';
-$skladky->id_skladka_typ = 2;
-$skladky->pocet_potvrdeni = 2;
+$skladkaTypCross->id_skladka = 2;
+$skladkaTypCross->id_skladka_typ = 1;
+$skladkaTypCross->pocet_potvrdeni = 10;
+
+R::store($skladkaTypCross);
+
+/** UCM_UNKNOWN_USERS */
+$ucmUnknownUsers1 = R::ucmdispense('ucm_unknown_users');
+
+$reportUserUid = uniqid();
+$ucmUnknownUsers1->uid = $reportUserUid;
+$ucmUnknownUsers1->created_at = date('Y-m-d H:i:s', time());
+$ucmUnknownUsers1->last_login = date('Y-m-d H:i:s', time());
+
+R::store($ucmUnknownUsers1);
+
+$ucmUnknownUsers2 = R::ucmdispense('ucm_unknown_users');
+
+$acceptUserUid = uniqid();
+$ucmUnknownUsers2->uid = $acceptUserUid;
+$ucmUnknownUsers2->created_at = date('Y-m-d H:i:s', time());
+$ucmUnknownUsers2->last_login = date('Y-m-d H:i:s', time());
+
+R::store($ucmUnknownUsers2);
+
+/** UCM_SKLADKY_UNKNOWN_USERS */
+$ucmSkladkyUnknownUsers = R::ucmdispense('ucm_skladky_unknown_users');
+
+$ucmSkladkyUnknownUsers->id_skladka = 2;
+$ucmSkladkyUnknownUsers->unknown_user_uid = $reportUserUid;
+
+R::store($ucmSkladkyUnknownUsers);
+
+/** UCM_SKLADKY_POTVRDENIA */
+$ucmSkladkyPotvrdenia = R::ucmdispense('ucm_skladky_potvrdenia');
+
+$ucmSkladkyPotvrdenia->id_skladka = 2;
+$ucmSkladkyPotvrdenia->unknown_user_uid = $acceptUserUid;
+
+R::store($ucmSkladkyPotvrdenia);
+
+
