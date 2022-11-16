2d3863d0656d82b6873ecdd05231afb698f54ca8
Holes70 <holespato@gmail.com>
2022-11-16 17:09:29 +0100

    imageupload test

---
+++ tests/image-upload.php
@@ -0,0 +1,10 @@
+<form action="../index.php?page=nelegalna-skladka-nahrat-obrazok" method="post" enctype="multipart/form-data">
+  <p>
+    <input type="file" name="image1" />
+    <input type="file" name="image2" />
+    <input type="file" name="image3" />
+    <input type="text" name="idSkladka" value="33"/>
+    <input type="text" name="uid" value="xxx"/>
+    <input type="submit" value="Submit" name="submit"/>
+  </p>
+</form>
